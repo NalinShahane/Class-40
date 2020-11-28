37b2dc9940b3ea4bbc78dd3abe28c235d5b63988
NalinShahane <70761427+NalinShahane@users.noreply.github.com>
2020-11-28 17:50:55 +0530

    Add files via upload

---
+++ js/Player.js
@@ -0,0 +1,38 @@
+class Player {
+  constructor(){
+    this.index = null;
+    this.distance = 0;
+    this.name = null;
+  }
+
+  getCount(){
+    var playerCountRef = database.ref('playerCount');
+    //arrow (=>) is Used for original Functions 
+    playerCountRef.on("value",(data) => {
+      playerCount = data.val();
+    })
+  }
+
+  updateCount(count){
+    database.ref('/').update({
+      playerCount: count
+    });
+  }
+
+  update(){
+    var playerIndex = "players/player" + this.index;
+    database.ref(playerIndex).set({ 
+      name: this.name,
+      distance: this.distance
+    });
+  }
+
+  static getPlayerInfo(){
+    var playerInfoRef = database.ref('players');
+    playerInfoRef.on("value",(data)=>{
+      allPlayers = data.val();
+    });
+  } 
+}
+
+//arrow (=>) is Used for original Functions 
